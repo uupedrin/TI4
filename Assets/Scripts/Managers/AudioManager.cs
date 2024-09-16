b98fd595280190b387679acce250e10f95527abb
uupedrin <pedroveiga.ribeiro@gmail.com>
2024-09-16 08:02:54 -0300

    feat: Audio Manager saving audio preferences

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+	AudioMixer _mixer;
+	private float masterVol, musicVol, sfxVol;
+	
+	private void Awake()
+	{
+		_mixer = Resources.Load("MainMixer") as AudioMixer;
+	}
+	
+	public void SetMaster(float value)
+	{
+		masterVol = value;
+		SetMixer();
+		SaveAudioPrefs();
+	}
+	public void SetMusic(float value)
+	{
+		musicVol = value;
+		SetMixer();
+		SaveAudioPrefs();
+	}
+	public void SetSFX(float value)
+	{
+		sfxVol = value;
+		SetMixer();
+		SaveAudioPrefs();
+	}
+	
+	void SetMixer()
+	{
+		_mixer.SetFloat("MasterVol", masterVol);
+		_mixer.SetFloat("MusicVol", musicVol);
+		_mixer.SetFloat("SFXVol", sfxVol);
+	}
+	
+	void LoadAudioPrefs()
+	{
+		masterVol = PlayerPrefs.GetFloat("MasterVol", 0);
+		musicVol = PlayerPrefs.GetFloat("MusicVol", 0);
+		sfxVol = PlayerPrefs.GetFloat("SFXVol", 0);
+	}
+	
+	void SaveAudioPrefs()
+	{
+		PlayerPrefs.SetFloat("MasterVol", masterVol);
+		PlayerPrefs.SetFloat("MusicVol", musicVol);
+		PlayerPrefs.SetFloat("SFXVol", sfxVol);
+	}
+}
