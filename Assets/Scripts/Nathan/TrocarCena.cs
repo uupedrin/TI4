4e9b1a3561e82e21a0d70aaca2e5cf41804f11e4
Corvus037 <nathanof2003@outlook.com>
2024-09-21 01:04:50 -0300

    Animaçao da Porta

--- Assets/Scripts/Nathan/TrocarCena.cs
+++ Assets/Scripts/Nathan/TrocarCena.cs
@@ -1,16 +1,45 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-
 
 public class TrocarCena : MonoBehaviour
 {
     public string sceneName;
+    public Animator doorAnimator; 
+    public Animator cameraAnimator; 
+    public string doorAnimationTrigger = "Porta"; 
+    public string cameraAnimationTrigger = "CameraApproach"; 
+    public float animationDuration = 1.5f;
+
+    private bool isSceneChanging = false;
+
     void OnMouseDown()
     {
+        if (!isSceneChanging)
+        {
+            isSceneChanging = true;
+            StartCoroutine(PlayAnimationsAndChangeScene());
+        }
+    }
+
+    IEnumerator PlayAnimationsAndChangeScene()
+    {
         
+        if (doorAnimator != null)
+        {
+            doorAnimator.SetTrigger(doorAnimationTrigger);
+        }
+
+        
+        if (cameraAnimator != null)
+        {
+            cameraAnimator.SetTrigger(cameraAnimationTrigger);
+        }
+
+        
+        yield return new WaitForSeconds(animationDuration);
+
+       
         SceneManager.LoadScene(sceneName);
     }
 }
