32f6ea8a3a6efe783de2a884513cfafc1d1a3414
Corvus037 <nathanof2003@outlook.com>
2024-10-19 13:38:55 -0300

    Game Over com o laser

---
+++ Assets/Scripts/Nathan/BotaoBasico.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotaoBasico : MonoBehaviour
+{
+    public string sceneName;  
+
+    
+    public void ChangeScene()
+    {
+        
+        SceneManager.LoadScene(sceneName);
+    }
+}
