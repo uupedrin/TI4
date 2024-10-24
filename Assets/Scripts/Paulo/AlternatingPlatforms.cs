f02d0abcc0d262251fc6eed0400de929636c728f
Paulo Campos Dias <paulo.campos.sou@gmail.com>
2024-10-24 16:27:23 -0300

    Made the alternating platforms more flexible.

--- Assets/Scripts/Paulo/AlternatingPlatforms.cs
+++ Assets/Scripts/Paulo/AlternatingPlatforms.cs
@@ -4,11 +4,45 @@ using UnityEngine;
 
 public class AlternatingPlatforms : MonoBehaviour
 {
+    public GameObject[] platformGroup;
+    public Material activeMaterial;
+    public Material inactiveMaterial;
+    int currentGroup;
+
+    void Start()
+    {
+        currentGroup = 0;
+        for(int i = 0; i < platformGroup[0].transform.childCount; i++)
+        {
+            PlatformTreatment(0, i, true, activeMaterial);
+        }  
+        for(int i = 1; i < platformGroup.Length; i++)
+        {
+            for(int j = 0; j < platformGroup[i].transform.childCount; j++)
+            {
+                PlatformTreatment(i, j, false, inactiveMaterial);
+            }  
+        }
+    }
+
     public void Alternate()
     {
-        for(int i = 0; i < transform.childCount; i++)
+        for(int i = 0; i < platformGroup[currentGroup].transform.childCount; i++)
         {
-            transform.GetChild(i).gameObject.SetActive(!transform.GetChild(i).gameObject.activeInHierarchy);
+            PlatformTreatment(currentGroup, i, false, inactiveMaterial);
         }
+        currentGroup++;
+        currentGroup %= platformGroup.Length;
+        for(int i = 0; i < platformGroup[currentGroup].transform.childCount; i++)
+        {
+            PlatformTreatment(currentGroup, i, true, activeMaterial);
+        }        
+    }
+
+    public void PlatformTreatment(int group, int position, bool activity, Material mat)
+    {
+        platformGroup[group].transform.GetChild(position).GetChild(0).GetComponent<Renderer>().material = mat;
+        platformGroup[group].transform.GetChild(position).GetComponent<BoxCollider>().enabled = activity;
+        platformGroup[group].transform.GetChild(position).GetComponent<Rigidbody>().isKinematic = !activity;
     }
 }
