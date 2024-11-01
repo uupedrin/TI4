51a8d7f47efe570d14f85962278f3a6a0f4c6a87
Lucas Henrique <130869255+pumpedkk@users.noreply.github.com>
2024-11-01 10:09:30 -0300

    Player Pulo

--- Assets/PlayerPrefab/PlayerController.cs
+++ Assets/PlayerPrefab/PlayerController.cs
@@ -12,6 +12,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private bool PuloDoploAbl;
     [SerializeField] private bool correAbl;
     [SerializeField] private bool rolamentoAbl;
+    [SerializeField] private bool podeMover;
+    [SerializeField] private bool gravidadeBool;
 
     #endregion
     #region Movimento Var
@@ -36,52 +38,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private bool podePuloDoplo;
     public bool queda = false;
     #endregion
-    #region Rolamento var
-    [Header("Config Rolamento")]
-    public bool rolamento = false;
-    [SerializeField] float colliderRolamento;
-    [SerializeField] float alturaRolamento;
-    private float colliderPrincipal;
-    private float alturaPrincipal;
-    [SerializeField] AnimationCurve rolamentoCurva;
-    private float tempoRolamento;
-    [SerializeField] float velocidadeRolamento;
-    [SerializeField] float tempoCollider;
-    #endregion
-    #region Agarrar var
-    [Header("Config Agarrar")]
-    [SerializeField] bool agarrando;
-    [SerializeField] bool podeMover = true;
-    [SerializeField] bool gravidadeBool = true;
-    [SerializeField] float A;
-    [SerializeField] float B;
-    float gravidadeAntiga;
-    #endregion
-    #region Escada
-    [Header("Escada")]
-    [SerializeField] float velocidadeEscada;
-    public bool escada;
-    #endregion
-    #region Dash
-    [Header("Dash")]
-    [SerializeField] float DashTempo;
-    [SerializeField] float DashSpeed;
-    [SerializeField] bool Dash;
-    #endregion
-    #region WallJump
-    [Header("WallJump")]
-    [SerializeField] float gravidadeParede;
-    [SerializeField] LayerMask raycastLayerMask;
-    [SerializeField] float tamanhoRaycast;
-    [SerializeField] bool parede;
-    private string nomeParedeAnterior;
-    #endregion
     #region Verificacao
     [Header("Verificacão")]
-    [SerializeField] float timerRolamento;
-    [SerializeField] float velY;
-    [SerializeField] bool puloMaximo;
-
     GameObject alternatingPlatforms;
     AlternatingPlatforms alt;
     #endregion
@@ -96,12 +54,6 @@ public class PlayerController : MonoBehaviour
         }
         velocidadeAntiga = velocidade;
         characterController = GetComponent<CharacterController>();
-        Keyframe rolamento_ultimoFrame = rolamentoCurva[rolamentoCurva.length - 1];
-        tempoRolamento = rolamento_ultimoFrame.time - 0.15f;
-        gravidadeAntiga = gravidade;
-        colliderPrincipal = characterController.height;
-        alturaPrincipal = characterController.center.y;
-
     }
 
 
@@ -113,34 +65,16 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
 
-
-        velY = (int)velocidadeMovimento.y;
-        if (velY >= 1f)
-        {
-
-            puloMaximo = true;
-        }
-        if (wallJump)
-            ParedePulo();
-        if (AgarrarAbl)
-            Agarrar();
-        if (escada)
-        {
-            velocidadeMovimento.y = myInput.y * velocidadeEscada;
-            animator.speed = Mathf.Abs(myInput.y);
-        }
-
         chao = NoChao();
         animator.SetBool("Chao", chao);
-        animator.SetBool("Parede", parede);
 
+        if (gravidadeBool)
+        {
+            velocidadeMovimento.y -= gravidade * Time.deltaTime;
+        }
 
         if (NoChao())
         {
-
-            nomeParedeAnterior = null;
-            puloMaximo = false;
-            Dash = true;
             caindo = false;
             podePuloDoplo = true;
             animator.SetBool("Caindo", false);
@@ -159,17 +93,14 @@ public class PlayerController : MonoBehaviour
         else
             velocidade = velocidadeAntiga;
 
-        if (!rolamento && podeMover)
+        if (podeMover)
             characterController.Move(transform.forward * myInput.magnitude * velocidade * Time.deltaTime);
 
         if (chao && velocidadeMovimento.y < 0)
         {
             velocidadeMovimento.y = -2f; // Pequeno valor negativo para garantir que o characterController está no chão
         }
-        if (gravidadeBool)
-        {
-            velocidadeMovimento.y -= gravidade * Time.deltaTime;
-        }
+       
         characterController.Move(velocidadeMovimento * Time.deltaTime);
         animator.SetBool("Walk", myInput != Vector2.zero);
     }
@@ -181,7 +112,7 @@ public class PlayerController : MonoBehaviour
         Vector3 right = myCamera.TransformDirection(Vector3.right);
         Vector3 targetDirection = myInput.x * right + myInput.y * forward;
 
-        if (myInput != Vector2.zero && targetDirection.magnitude > 0.1f && !rolamento && podeMover)
+        if (myInput != Vector2.zero && targetDirection.magnitude > 0.1f && podeMover)
         {
             Quaternion freeRotation = Quaternion.LookRotation(targetDirection.normalized);
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(new Vector3(transform.eulerAngles.x, freeRotation.eulerAngles.y, transform.eulerAngles.z)), 10 * Time.deltaTime);
@@ -199,47 +130,9 @@ public class PlayerController : MonoBehaviour
 
     public void Pulo(InputAction.CallbackContext value)
     {
-        if (rolamento)
-            return;
-        if (value.started && agarrando)
+        if (value.started && NoChao())
         {
-            parede = false;
-            Dash = true;
-            Invoke("PodeMover", 0.35f);
-            gravidade = gravidadeAntiga;
-            agarrando = false;
-            animator.SetTrigger("Pulo");
-            pulo = true;
-            velocidadeMovimento.y = Mathf.Sqrt((puloAltura * 2) * 2 * gravidade);
-            alt.Alternate();
-        }
-        else if (value.started && escada)
-        {
-            SairEscada();
-            Dash = true;
-            velocidadeMovimento.y = Mathf.Sqrt((puloAltura * 2) * 2 * gravidade);
-            alt.Alternate();
-        }
-        else if (value.started && !NoChao() && parede)
-        {
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.forward, out hit, tamanhoRaycast, raycastLayerMask))
-                nomeParedeAnterior = hit.collider.name;
-            Dash = true;
-            animator.SetTrigger("Pulo");
-            pulo = true;
-            podeMover = true;
-            parede = false;
-            velocidadeMovimento = Vector3.zero;
-            transform.forward = transform.forward * -1;
-            gravidade = gravidadeAntiga;
-            velocidadeMovimento.y = Mathf.Sqrt(puloAltura * 2 * gravidade);
-            alt.Alternate();
-
-        }
-        else if (value.started && NoChao())
-        {
-            Dash = true;
+            
             animator.SetTrigger("Pulo");
             pulo = true;
             velocidadeMovimento.y = Mathf.Sqrt(puloAltura * 2 * gravidade);
@@ -247,7 +140,7 @@ public class PlayerController : MonoBehaviour
         }
         else if (value.started && !NoChao() && podePuloDoplo && PuloDoploAbl)
         {
-            Dash = true;
+            
             animator.SetTrigger("Pulo");
             pulo = true;
             velocidadeMovimento = Vector3.zero;
@@ -255,166 +148,15 @@ public class PlayerController : MonoBehaviour
             alt.Alternate();
             podePuloDoplo = false;
         }
+        else if(value.canceled)
+        velocidadeMovimento = Vector3.zero;
 
     }
-    public void RolamentoInput(InputAction.CallbackContext value)
-    {
-        if (!rolamento && rolamentoAbl)
-            StartCoroutine(Rolamento());
-    }
-    IEnumerator Rolamento()
-    {
-        if (!agarrando && NoChao())
-        {
-
-            animator.SetTrigger("Rolamento");
-            rolamento = true;
-            timerRolamento = 0;
-
-
-            while (timerRolamento < tempoRolamento)
-            {
-                if (timerRolamento >= tempoCollider)
-                {
-                    characterController.height = colliderRolamento;
-                    characterController.center = new Vector3(0, alturaRolamento, 0);
-                }
-
-                float speed = rolamentoCurva.Evaluate(timerRolamento);
-                Vector3 dir = (transform.forward * velocidadeRolamento) + Vector3.up * velocidadeMovimento.y;
-                characterController.Move(dir * Time.deltaTime);
-                timerRolamento += Time.deltaTime;
-                yield return null;
-            }
-            rolamento = false;
-            characterController.height = colliderPrincipal;
-            characterController.center = new Vector3(0, alturaPrincipal, 0);
-        }
-        if (!agarrando && !NoChao() && Dash)
-        {
-            rolamento = true;
-            timerRolamento = 0;
-            Dash = false;
-
-
-            while (timerRolamento < DashTempo)
-            {
-                if (timerRolamento >= tempoCollider)
-                {
-                    characterController.height = colliderRolamento;
-                    characterController.center = new Vector3(0, alturaRolamento, 0);
-                }
-
-                float speed = rolamentoCurva.Evaluate(timerRolamento);
-                gravidadeBool = false;
-                Vector3 dir = (transform.forward * DashSpeed);
-                velocidadeMovimento = Vector3.zero;
-                characterController.Move(dir * Time.deltaTime);
-                timerRolamento += Time.deltaTime;
-                yield return null;
-            }
-            gravidadeBool = true;
-            rolamento = false;
-            characterController.height = colliderPrincipal;
-            characterController.center = new Vector3(0, alturaPrincipal, 0);
-        }
-    }
-
     public bool NoChao() => characterController.isGrounded;
 
     void PodeMover()
     {
         podeMover = true;
-    }
-    public void SairEscada()
-    {
-        gravidade = gravidadeAntiga;
-        gravidadeBool = true;
-        escada = false;
-        podeMover = true;
-        animator.SetBool("Escada", false);
-        animator.speed = 1;
-    }
-
-    void Agarrar()
-    {
-        if (velocidadeMovimento.y < 0 && !agarrando)
-        {
-            RaycastHit downHit;
-            Vector3 lineDownStart = (transform.position + Vector3.up * A) + (transform.forward * 0.5f);
-            Vector3 lineDownEnd = (transform.position + Vector3.up * B) + (transform.forward * 0.5f);
-            Physics.Linecast(lineDownStart, lineDownEnd, out downHit, LayerMask.GetMask("Plataformas"));
-            Debug.DrawLine(lineDownStart, lineDownEnd);
-
-            if (downHit.collider != null && caindo)
-            {
-                RaycastHit fwdHit;
-                Vector3 lineFwdStart = new Vector3(transform.position.x, downHit.point.y, transform.position.z);
-                Vector3 lineFwdEnd = new Vector3(transform.position.x, downHit.point.y, transform.position.z) + transform.forward;
-                Physics.Linecast(lineFwdStart, lineFwdEnd, out fwdHit, LayerMask.GetMask("Plataformas"));
-                Debug.DrawLine(lineFwdStart, lineFwdEnd);
-
-                if (fwdHit.collider != null)
-                {
-                    parede = true;
-                    animator.SetTrigger("Agarrar");
-                    gravidade = 0;
-                    podeMover = false;
-                    agarrando = true;
-                    velocidadeMovimento.y = 0;
-                    Vector3 hangPos = new Vector3(fwdHit.point.x, downHit.point.y, fwdHit.point.z);
-                    Vector3 offset = transform.forward * -0.1f + transform.up * -0.1f;
-                    hangPos += offset;
-                    transform.position = hangPos * -1;
-                    transform.forward = fwdHit.normal * -1;
-                }
-            }
-        }
-    }
-    public void Escada()
-    {
-        escada = true;
-        animator.SetBool("Escada", true);
-        podeMover = false;
-        gravidadeBool = false;
-
-    }
-    void ParedePulo()
-    {
-        Debug.DrawRay(transform.position, transform.forward * tamanhoRaycast, Color.red);
-
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, tamanhoRaycast, raycastLayerMask))
-        {
-            if (!NoChao() && puloMaximo && hit.collider.name != nomeParedeAnterior)
-            {
-                if (velocidadeMovimento.y > 0)
-                    velocidadeMovimento.y = 0;
-                podePuloDoplo = true;
-                animator.SetTrigger("Agarrar");
-                transform.forward = hit.normal * -1;
-                gravidade = gravidadeParede + Time.deltaTime;
-                Debug.DrawRay(transform.position, transform.forward * tamanhoRaycast, Color.green);
-                podeMover = false;
-                parede = true;
-            }
-            else
-            {
-                podeMover = true;
-                parede = false;
-                gravidade = gravidadeAntiga;
-            }
-
-
-        }
-    }
-    public void PuloCaixa()
-    {
-        Dash = true;
-        animator.SetTrigger("Pulo");
-        pulo = true;
-        velocidadeMovimento.y = Mathf.Sqrt(puloAltura * 2 * gravidade);
-        alt.Alternate();
     }
 }
 
