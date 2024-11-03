56409f888047d1f2c0da6c0cff54ae5564f42c45
Corvus037 <nathanof2003@outlook.com>
2024-11-02 21:54:37 -0300

    game over concertado

---
+++ Assets/Scripts/Nathan/Mouse.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    void Start()
+    {
+        
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
