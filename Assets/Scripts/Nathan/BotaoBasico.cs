6d4621ece82fb83d69b01c55403b1f9c7efd8510
Vinicius-commits <viniciusnp3610@gmail.com>
2024-11-28 00:35:03 -0300

    savefile

--- Assets/Scripts/Nathan/BotaoBasico.cs
+++ Assets/Scripts/Nathan/BotaoBasico.cs
@@ -10,7 +10,7 @@ public class BotaoBasico : MonoBehaviour
     
     public void ChangeScene()
     {
-        
+        GameManager.instance.Save();
         SceneManager.LoadScene(sceneName);
     }
 }
