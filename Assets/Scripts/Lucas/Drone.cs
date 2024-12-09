d308ad1c7131c0b81c51e454b07648457f829dde
d0lynnho <163213054+d0lynnho@users.noreply.github.com>
2024-12-09 02:09:16 -0300

    baia

--- Assets/Scripts/Lucas/Drone.cs
+++ Assets/Scripts/Lucas/Drone.cs
@@ -9,141 +9,148 @@ using Unity.VisualScripting;
 public class Drone : MonoBehaviour
 {
 	[SerializeField] GameObject _mesa, prefab;
-	[SerializeField] float _speed;
-	[SerializeField] Transform MeioSala, mesaSpawn;
-	[SerializeField] private bool indo, voltando, meio, colocando;
-	[SerializeField] private Transform transformAtual;
-	[SerializeField] private float timeEspera;
-	private float startTime;
-	private Vector3 startPos;
-	private int p;
-	[SerializeField] private Vector3 startT;
-	[SerializeField] private int maxMesas = 5;
-	private int mesasInstanciadas = 0;
-	[SerializeField] private TextMeshProUGUI mesasText;
-	private List<GameObject> mesasCriadas = new List<GameObject>(); 
-	[SerializeField] private List<Transform> lugares = new List<Transform>();
-	
-	AudioSource source;
-	
-	private Transform atual;
+[SerializeField] float _speed;
+[SerializeField] Transform MeioSala, mesaSpawn;
+[SerializeField] private bool indo, voltando, meio, colocando;
+[SerializeField] private Transform transformAtual;
+[SerializeField] private float timeEspera;
+private float startTime;
+private Vector3 startPos;
+private int p;
+[SerializeField] public Vector3 startT;
+[SerializeField] private int maxMesas = 5;
+private int mesasInstanciadas = 0;
+[SerializeField] private TextMeshProUGUI mesasText;
+private List<GameObject> mesasCriadas = new List<GameObject>(); 
+[SerializeField] private List<Transform> lugares = new List<Transform>();
 
-	void Awake()
+AudioSource source;
+
+private Transform atual;
+
+void Awake()
+{
+	startT = transform.position;
+	_mesa = Instantiate(prefab, mesaSpawn.position, mesaSpawn.rotation, transform);
+	mesasCriadas.Add(_mesa);
+	mesasInstanciadas++;
+	source = GetComponent<AudioSource>();
+	source.enabled = false;
+}
+
+void Start(){
+	AtualizarTextoMesas();
+
+}
+void Update()
+{
+
+}
+
+public void Colocar(Transform Pos)
+{
+	if(colocando)
 	{
-		startT = transform.position;
-		_mesa = Instantiate(prefab, mesaSpawn.position, mesaSpawn.rotation, transform);
-		mesasCriadas.Add(_mesa);
-		mesasInstanciadas++;
-		source = GetComponent<AudioSource>();
-		source.enabled = false;
+		lugares.Add(Pos);
+		atual = Pos;
 	}
-
-	void Start(){
-		AtualizarTextoMesas();
-
-	}
-	void Update()
+	else
 	{
-
+		transformAtual = Pos;
+		startPos = transform.position;
+		meio = true;
+		colocando = true;
+		StartCoroutine(MoveObject());
 	}
+}
 
-	public void Colocar(Transform Pos)
+IEnumerator MoveObject()
+{
+	source.enabled = true;
+	while (meio || indo || voltando)
 	{
-		if(colocando)
+		if (meio)
 		{
-			lugares.Add(Pos);
-			atual = Pos;
-		}
-		else
-		{
-			transformAtual = Pos;
-			startPos = transform.position;
-			meio = true;
-			colocando = true;
-			StartCoroutine(MoveObject());
-		}
-	}
-
-	IEnumerator MoveObject()
-	{
-		source.enabled = true;
-		while (meio || indo || voltando)
-		{
-			if (meio)
+			transform.position = Vector3.MoveTowards(transform.position, MeioSala.position, _speed * Time.deltaTime);
+			if (Vector3.Distance(transform.position, MeioSala.position) < 0.01f)
 			{
-				transform.position = Vector3.MoveTowards(transform.position, MeioSala.position, _speed * Time.deltaTime);
-				if (Vector3.Distance(transform.position, MeioSala.position) < 0.01f)
+				meio = false;
+				indo = true;
+				startPos = transform.position;
+				yield return new WaitForSeconds(timeEspera);
+			}
+		}
+		else if (indo)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, transformAtual.position, _speed * Time.deltaTime);
+			if (Vector3.Distance(transform.position, transformAtual.position) < 0.01f)
+			{
+				voltando = true;
+				indo = false;
+				startPos = transform.position;
+				_mesa.transform.SetParent(null);
+				_mesa.GetComponent<Rigidbody>().useGravity = true;
+				yield return new WaitForSeconds(timeEspera);
+			}
+		}
+		else if (voltando)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, startT, _speed * Time.deltaTime);
+			if (Vector3.Distance(transform.position, startT) < 0.01f)
+			{
+				voltando = false;
+				startPos = transform.position;
+				colocando = false;
+				if (mesasInstanciadas < maxMesas)
 				{
-					meio = false;
-					indo = true;
-					startPos = transform.position;
-					yield return new WaitForSeconds(timeEspera);
+					_mesa = Instantiate(prefab, mesaSpawn.position, mesaSpawn.rotation, transform);
+					mesasCriadas.Add(_mesa);
+					mesasInstanciadas++;
+					AtualizarTextoMesas();
+				}
+				else
+				{
+					Debug.Log("Número máximo de mesas instanciadas atingido.");
 				}
 			}
-			else if (indo)
-			{
-				transform.position = Vector3.MoveTowards(transform.position, transformAtual.position, _speed * Time.deltaTime);
-				if (Vector3.Distance(transform.position, transformAtual.position) < 0.01f)
-				{
-					voltando = true;
-					indo = false;
-					startPos = transform.position;
-					_mesa.transform.SetParent(null);
-					_mesa.GetComponent<Rigidbody>().useGravity = true;
-					yield return new WaitForSeconds(timeEspera);
-				}
-			}
-			else if (voltando)
-			{
-				transform.position = Vector3.MoveTowards(transform.position, startT, _speed * Time.deltaTime);
-				if (Vector3.Distance(transform.position, startT) < 0.01f)
-				{
-					voltando = false;
-					startPos = transform.position;
-					colocando = false;
-					if (mesasInstanciadas < maxMesas)
-					{
-						_mesa = Instantiate(prefab, mesaSpawn.position, mesaSpawn.rotation, transform);
-						mesasCriadas.Add(_mesa);
-						mesasInstanciadas++;
-						AtualizarTextoMesas();
-					}
-					else
-					{
-						Debug.Log("Número máximo de mesas instanciadas atingido.");
-					}
-				}
-			}
-			yield return null;
-			
 		}
-		source.enabled = false;
-		if(lugares.Count != 0 && !colocando)
-			{
-				Colocar(lugares[0]);
-				lugares.RemoveAt(0);
-				yield return null;
-			}
 		yield return null;
-			
+		
 	}
-	public void DestruirUltimaMesa()
-	{
-		if (mesasCriadas.Count > 1) 
+	source.enabled = false;
+	if(lugares.Count != 0 && !colocando)
 		{
-			GameObject penultimaMesa = mesasCriadas[mesasCriadas.Count - 2];
-			mesasCriadas.RemoveAt(mesasCriadas.Count - 2); 
-			Destroy(penultimaMesa); 
-			mesasInstanciadas--; 
-			AtualizarTextoMesas(); 
+			Colocar(lugares[0]);
+			lugares.RemoveAt(0);
+			yield return null;
 		}
-		else
-		{
-			Debug.Log("Não há mesas suficientes para destruir a penúltima.");
-		}
-	}
+	yield return null;
+		
+}
+    public void DestruirUltimaMesa()
+    {
+        if (Vector3.Distance(transform.position, startT) > 0.01f)
+        {
+            Debug.Log("O drone não está na posição inicial. Não é possível destruir a penúltima mesa.");
 
-	private void AtualizarTextoMesas()
+        }
+
+        if (mesasCriadas.Count > 1)
+        {
+            GameObject penultimaMesa = mesasCriadas[mesasCriadas.Count - 1];
+            mesasCriadas.RemoveAt(mesasCriadas.Count - 1);
+            Debug.Log("Removendo mesa: " + penultimaMesa.name);
+            Destroy(penultimaMesa);
+            mesasInstanciadas--;
+            AtualizarTextoMesas();
+        }
+        else
+        {
+            Debug.Log("Não há mesas suficientes para destruir a penúltima.");
+        }
+    }
+
+    private void AtualizarTextoMesas()
 	{
 		mesasText.text = "Mesas: " + mesasInstanciadas + "/" + maxMesas;
 	}
--- Assets/Scripts/Nathan/ContadorBotao.cs
+++ Assets/Scripts/Nathan/ContadorBotao.cs
@@ -7,93 +7,100 @@ using TMPro;
 public class ContadorBotao : MonoBehaviour
 {
     public int maxClicks = 6; // Número máximo de cliques
-    public int clickCount = 0; // Contador de cliques
-    public Button[] buttons; // Botões para gerenciar
-    public Button resetButton; // Botão para resetar
-    public Sprite newSprite; // Sprite para alterar botões
-    public TextMeshProUGUI clickCountText; // Referência para o TextMeshPro na UI
+public int clickCount = 0; // Contador de cliques
+public Button[] buttons; // Botões para gerenciar
+public Button resetButton; // Botão para resetar
+public Sprite newSprite; // Sprite para alterar botões
+public TextMeshProUGUI clickCountText; // Referência para o TextMeshPro na UI
 
-    private List<Button> clickedButtons = new List<Button>(); // Lista de botões clicados
-    public Drone drone; // Referência ao script do Drone
+private List<Button> clickedButtons = new List<Button>(); // Lista de botões clicados
+public Drone drone; // Referência ao script do Drone
 
-    void Start()
+void Start()
+{
+    foreach (Button button in buttons)
     {
-        foreach (Button button in buttons)
-        {
-            button.onClick.AddListener(() => OnButtonClicked(button));
-        }
-
-        if (resetButton != null)
-        {
-            resetButton.onClick.AddListener(OnResetButtonClicked);
-        }
-
-        UpdateClickCountText();
+        button.onClick.AddListener(() => OnButtonClicked(button));
     }
 
-    void OnButtonClicked(Button button)
+    if (resetButton != null)
     {
-        clickCount++;
+        resetButton.onClick.AddListener(OnResetButtonClicked);
+    }
 
-        if (!clickedButtons.Contains(button))
+    UpdateClickCountText();
+}
+
+void OnButtonClicked(Button button)
+{
+    clickCount++;
+
+    if (!clickedButtons.Contains(button))
+    {
+        clickedButtons.Add(button);
+    }
+
+    UpdateClickCountText();
+
+    if (clickCount >= maxClicks)
+    {
+        foreach (Button btn in buttons)
         {
-            clickedButtons.Add(button);
+            btn.interactable = false;
+        }
+    }
+}
+
+void OnResetButtonClicked()
+{
+    // Verifica se o drone está na posição inicial
+    if (drone != null && Vector3.Distance(drone.transform.position, drone.startT) > 0.01f)
+    {
+        Debug.Log("O drone não está na posição inicial. Não é possível remover a última mesa.");
+        return;
+    }
+
+    if (clickCount > 0)
+    {
+        clickCount--;
+
+        // Remover a última mesa (e não a penúltima)
+        if (drone != null)
+        {
+            drone.DestruirUltimaMesa(); // Remover a última mesa colocada
         }
 
-        UpdateClickCountText();
+        // Alterar o sprite do botão após a remoção da mesa
+        if (clickedButtons.Count > 0)
+        {
+            // Alterar o sprite do último botão clicado
+            Button lastClickedButton = clickedButtons[clickedButtons.Count - 1];
+            Image buttonImage = lastClickedButton.GetComponent<Image>();
+            if (buttonImage != null && newSprite != null)
+            {
+                buttonImage.sprite = newSprite;
+            }
 
-        if (clickCount >= maxClicks)
+            clickedButtons.RemoveAt(clickedButtons.Count - 1);
+        }
+
+        if (clickCount < maxClicks)
         {
             foreach (Button btn in buttons)
             {
-                btn.interactable = false;
+                btn.interactable = true;
             }
         }
-    }
 
-    void OnResetButtonClicked()
+        UpdateClickCountText();
+    }
+}
+
+void UpdateClickCountText()
+{
+    if (clickCountText != null)
     {
-        if (clickCount > 0)
-        {
-            clickCount--;
-
-            // Remover a última mesa (e não a penúltima)
-            if (drone != null)
-            {
-                drone.DestruirUltimaMesa(); // Remover a última mesa colocada
-            }
-
-            // Alterar o sprite do botão após a remoção da mesa
-            if (clickedButtons.Count > 0)
-            {
-                // Alterar o sprite do último botão clicado
-                Button lastClickedButton = clickedButtons[clickedButtons.Count - 1];
-                Image buttonImage = lastClickedButton.GetComponent<Image>();
-                if (buttonImage != null && newSprite != null)
-                {
-                    buttonImage.sprite = newSprite;
-                }
-
-                clickedButtons.RemoveAt(clickedButtons.Count - 1);
-            }
-
-            if (clickCount < maxClicks)
-            {
-                foreach (Button btn in buttons)
-                {
-                    btn.interactable = true;
-                }
-            }
-
-            UpdateClickCountText();
-        }
+        clickCountText.text = $"Mesas: {clickCount}/{maxClicks}";
     }
-
-    void UpdateClickCountText()
-    {
-        if (clickCountText != null)
-        {
-            clickCountText.text = $"Mesas: {clickCount}/{maxClicks}";
-        }
-    }
+}
 }
